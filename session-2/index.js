58ebe6151f5e1c0ecb45134d4aeef296a1f6ccd3
Ikraan <ikram2009@hotmail.com>
2022-10-20 20:42:24 +0100

    adding session 2

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,7 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+//console.log("Hello Everyone")
+
+//console.log('InnerHTML:', document.querySelector('.about').innerHTML)
+
+//console.log('All P elements:',document.querySelectorAll('.about'))
